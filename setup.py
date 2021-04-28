6f78ae45e894c2c26200dac4b455544bea3322b2
MichaelNasello <mlnasell@uwaterloo.ca>
2021-04-28 11:54:37 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Neural Networks from Scratch",
+    version="1.0",
+    author="Michael Nasello",
+    install_requires=[
+        "numpy~=1.19.5",
+        "setuptools~=51.3.3",
+    ],
+    packages=find_packages()
+)
