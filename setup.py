5f1aa08eea8285b3a7a4cb927740241dfc1ea043
MichaelNasello <mlnasell@uwaterloo.ca>
2021-04-28 14:54:23 -0400

    Updated package requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version="1.0",
     author="Michael Nasello",
     install_requires=[
+        "requests~=2.25.1",
+        "matplotlib~=3.4.1",
+        "Pillow~=8.2.0",
         "numpy~=1.19.5",
         "setuptools~=51.3.3",
     ],
